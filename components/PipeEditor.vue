3c8f09dacb051b4c0fb23216d1b2665d711cb82b
Sam Thorogood <sam.thorogood@gmail.com>
2025-07-01 16:35:36 +1000

    remove unused

--- components/PipeEditor.vue
+++ components/PipeEditor.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
-import { ref, computed, watch } from 'vue';
+import { ref, watch } from 'vue';
 import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Checkbox } from '@/components/ui/checkbox';
